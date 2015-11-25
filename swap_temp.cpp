97fc3af99bbd89092353a4a6853bfd43ac452edc
GarfieldEr007 <guoerzq@gmail.com>
2015-11-25 19:07:18 +0800

    swap methods

---
+++ swap_temp.cpp
@@ -0,0 +1,18 @@
+void swap(int &a, int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;	
+}
+
+/*
+ * method II
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;	
+}
+*/
